aa57973bb53ec6097aa1ae4aabeaf410e0402c09
vallois <corentin.vallois@edu.itescia.fr>
2019-11-25 13:36:04 +0100

    story 3 missing diagrams

---
+++ src/DefaultArchetype.java
@@ -0,0 +1,73 @@
+public class DefaultArchetype {
+    private String name;
+    private int attack;
+    private  int life;
+    private  int initiative;
+
+    /**
+     * Archetype constructor.
+     * @see #name   string name of the archetype
+     * @see #attack int attack of the archetype
+     * @see #life   int life  of the archetype
+     * @see #initiative int initiative of the archetype
+     */
+
+    public DefaultArchetype (){
+        name= "unknow";
+        attack = 10;
+        life = 100;
+        initiative = 100;
+        }
+
+    /**
+     * Method getName.
+     * @return String name of the archetype
+     */
+    public String getName () {
+        return name;
+    }
+    /**
+     * Method getAttack.
+     * @return int Attack of the archetype
+     */
+    public int getAttack () {
+        return attack;
+    }
+    /**
+     * Method getLife.
+     * @return int life of the archetype
+     */
+    public int getLife () {
+        return life;
+    }
+    /**
+     * Method getInitiative.
+     * @return int initiative of the archetype
+     */
+    public int getInitiative () {
+        return initiative;
+    }
+    /**
+     * Method getDamageSend.
+     * @return int Damage send of the archetype
+     */
+    public int getDamageSend(){
+        int damage= (int) Math.round(Math.random()*(10-0) * attack);
+        return damage;
+    }
+    /**
+     * Method setDamageReceived.
+     * @param damageReceived quantity of damage received
+     * update life after receiving damage
+     */
+    public void setDamageReceived(int damageReceived){
+        life = life - damageReceived;
+    }
+    /**
+     * Method toString.
+     * @return String all archetype's attributes
+     */
+   public String toString() {
+        return name + " had " + life + " HP, " + attack + " strenght and " + initiative + " initiative";
+   }
+}
--- src/main.java
+++ src/main.java
@@ -1,5 +1,15 @@
 public class main {
+
+
+
+
     public static void main(String[] args) {
+        DefaultArchetype test = new DefaultArchetype();
+        test.setDamageReceived(15);
+        System.out.println(test.getLife());
+        System.out.println(test);
+
+
 
     }
 }
