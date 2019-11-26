3244b434e782b217edb6e92689f0a9734e9b7557
roman.clavier.2001@gmail.com <roman.clavier.2001@gmail.com>
2019-11-26 17:37:18 +0100

    story 10

--- src/ThiefArchetype.java
+++ src/ThiefArchetype.java
@@ -2,13 +2,37 @@ import java.util.Random;
 
 public class ThiefArchetype extends DefaultArchetype
 {
+    int critcalChanche;
+    Boolean canCritical;
+    int dodgeChance;
+    Boolean canDodge;
 
-    public ThiefArchetype () { archetypeName = "Thief"; }
+    public ThiefArchetype ()
+    {
+        archetypeName = "Thief";
+        critcalChanche = 10;
+        canCritical = true;
+        dodgeChance = 20;
+        canCritical = false;
+    }
 
     @Override
     public int setDamageReceived(int damageReceived)
     {
-        Commands.printn(characterName + "'s chibre reduced : " + (int)(damageReceived * 0.2) + " damage.");
+        this.canCritical = getCritical();
+        if (canCritical) {
+            int damageCritique = damageReceived * 2;
+            Commands.printn(characterName + " give a critical hit : " + damageCritique + " damage.");
+        } else {
+            Commands.printn(characterName + "'s give  : " + (int)(damageReceived * 0.2) + " damage.");
+        }
+
         return (int)(damageReceived * 0.8);
+    }
+
+    public Boolean getCritical() {
+    if (this.critcalChanche < new Random().nextInt(100) )
+        return true;
+    return false;
     }
 }
