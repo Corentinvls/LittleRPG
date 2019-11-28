392db696a177b5b47168358ead222f0187863a5a
Lrt95 <antony.correia@gmail.com>
2019-11-28 15:52:12 +0100

    correction Boolean

--- src/Commands.java
+++ src/Commands.java
@@ -9,7 +9,7 @@ public class Commands
      * Method to read command from Menu
      * @return true to continue or false to exit
      */
-    public static Boolean read()
+    public static boolean read()
     {
         Scanner sc = new Scanner(System.in);
         printn("*** Menu ***");
